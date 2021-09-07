0b52b6fff5fd6e5adee7a7818021c73c14dd706c
Akshaya Sanal <74820134+AKSHAYA-SANAL@users.noreply.github.com>
2021-09-07 16:59:33 +0530

    Update script.js

--- Image-Gallery/script.js
+++ Image-Gallery/script.js
@@ -18,3 +18,28 @@ function newbg(image) {             //image ==> this ==> img1/img2
 function oldbg(){
     document.getElementById("bg-img").style.backgroundImage = "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_PxLCGiziatDvotJ1OpIclPydDFDe-aqTzQ&usqp=CAU)"
 } 
+
+
+/*----------------------------------------Modal----------------------------------------*/
+var btn = document.getElementById("myBtn");
+
+var modal = document.getElementById("modal");
+
+//---------------------To display the Modal--------------------
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+var cross = document.getElementsByClassName("close")[0];
+
+//---------------------To hide the Modal when clicked on cross----------------------------
+cross.onclick = function() {
+    modal.style.display = "none";
+}
+
+//---------------------To hide the modal when clicked anywhere out--------------------------
+window.onclick = function(event) {
+    if (event.target == modal){
+    modal.style.display = "none";
+    }
+}
