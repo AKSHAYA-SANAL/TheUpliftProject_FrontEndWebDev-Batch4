02c5f32802d24de189c712dee63b1cd557e1a3de
Akshaya Sanal <74820134+AKSHAYA-SANAL@users.noreply.github.com>
2021-09-08 19:39:10 +0530

    Create script.js

---
+++ Image Gallery 2/script.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav(){
+    document.getElementById("sidenav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+}
